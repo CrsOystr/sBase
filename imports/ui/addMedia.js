f5d54435fc2d741b76dc91bd7229155d7babb77f
CrsOystr <nicmetz@jackmoves.com>
2016-04-01 08:45:45 -0600

    restructured and learned how to arned how to dynamically select images

--- imports/ui/addMedia.js
+++ imports/ui/addMedia.js
@@ -1,4 +1,17 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Websites } from '../api/websites.js'
+
 import './addMedia.html';
+
+Template.addMedia.onCreated(function addMediaCreated() {
+  Meteor.subscribe('websites');
+
+});
+
+Template.profileLink.helpers({
+    getMedia: function(){
+        var siteName = 'reddit';
+        return siteName;
+    }
+});
